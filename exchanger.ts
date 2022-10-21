2ca0b9701921f73868105c0c73defe5a006e7aa8
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-10-21 09:53:54 +0200

    exchanger.ts created and code added

---
+++ exchanger.ts
@@ -0,0 +1,11 @@
+class Exchanger {
+    gold : number;
+    silver : number;
+    copper : number;
+
+    constructor(gold : number, silver : number, copper : number;){
+        this.gold = gold;
+        this.silver = silver;
+        this.copper = copper;
+    }
+}
